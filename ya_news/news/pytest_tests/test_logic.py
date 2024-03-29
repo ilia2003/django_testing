45a88f44cec887d8aba9738c80e0e262b51bc896
Fedorenko Ilia <fedorenkoilia@gmail.com>
2024-03-29 17:07:10 +0100

    4

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -42,7 +42,7 @@ def test_create_comment_by_reader(news_detail_url, news, reader_client,
     )
     comments = Comment.objects.count() - comments_before
     assert comments == 1
-    comment = Comment.objects.last()
+    comment = Comment.objects.last
     assert comment.text == NEW_TEXT_FOR_COMMENTS['text']
     assert comment.news == news
     assert comment.author == reader
