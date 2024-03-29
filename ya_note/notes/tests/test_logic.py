2433e9e4fb03fc6fa212fe20582785ea0ad172cd
Fedorenko Ilia <fedorenkoilia@gmail.com>
2024-03-29 19:27:14 +0100

    4

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -36,7 +36,7 @@ class TestClass(TestBaseParameters):
     def test_anonymous_user_cant_create_note(self):
         notes_before = Note.objects.count()
         self.assertRedirects(
-            self.anonymous_client.post(Urls.NOTE_ADD, data=self.new_note_data),
+            self.anonymous_client.get(Urls.NOTE_ADD, data=self.new_note_data),
             Urls.REDIRECT_TO_NOTE_ADD
         )
         self.assertEqual(
@@ -53,7 +53,7 @@ class TestClass(TestBaseParameters):
             errors=self.new_note_data['slug'] + WARNING
         )
         self.assertEqual(
-            notes_before, set(Note.objects.all())
+            notes_before, Note.objects.count()
         )
 
     def test_author_can_edit(self):
@@ -72,7 +72,8 @@ class TestClass(TestBaseParameters):
     def test_author_can_delete_note(self):
         notes_before = Note.objects.count()
         self.author_client.delete(Urls.NOTE_DELETE)
-        self.assertEqual(Note.objects.count() - notes_before)
+        self.assertEqual(Note.objects.count(),
+                         Note.objects.count(notes_before) - 1)
         self.assertFalse(Note.objects.filter(pk=self.note.pk).exists())
 
     def test_reader_cant_delete_note(self):
